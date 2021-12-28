c8b690749d26b7bea9e7c4544ce32116e37d93e4
Arseni <1919ars@gmail.com>
2021-12-28 09:57:14 +0200

    more

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import asyncio
 from flask import Flask, redirect, url_for
 from flask import render_template
 from flask import request, session
+from interact_with_DB import interact_db
 
 app = Flask(__name__)
 app.secret_key = '123'
+
+# from pages.users.users import users
+# app.register_blueprint(users)
 
 
 @app.route('/try_request')
@@ -29,14 +33,12 @@ def hello_func():
 @app.route('/about')
 def about_func():
     # DO SOMETHING WITH DB
-    if session['username'] == '':
+    if 'username' not in session:
         return render_template('about.html')
     else:
         return render_template('about.html',
                                uni='BGU',
-                               profile={'name': 'Arseni',
-                                        'second_name': 'Perchik',
-                                        'middle_name': 'Ariel'},
+                               profile={'name': session['username'],},
                                degrees=['BSc.', 'MSc.'],
                                hobbies=('art', 'programming', 'teaching', 'horses', 'travel', 'music', 'sql'))
 
@@ -67,6 +69,88 @@ def logout_func():
     session['username'] = ''
     return redirect(url_for('login_func'))
 
+
+# ------------------------------------------------- #
+# -------------------- USERS ---------------------- #
+# ------------------------------------------------- #
+@app.route('/users')
+def users_func():
+    query = "select * from users"
+    query_result = interact_db(query=query, query_type='fetch')
+    return render_template('users.html', users=query_result)
+# ------------------------------------------------- #
+# ------------------------------------------------- #
+
+
+# @app.route('/hide_users')
+# def hide_users_func():
+#     session['users'] = ''
+#     return redirect(url_for('users_func'))
+
+
+# ------------------------------------------------- #
+# ------------------- SELECT ---------------------- #
+# ------------------------------------------------- #
+# @app.route('/select_users')
+# def select_users_func():
+#     query = "select * from users"
+#     query_result = interact_db(query=query, query_type='fetch')
+#     session['users'] = query_result
+#     return redirect(url_for('users_func'))
+
+
+# ------------------------------------------------- #
+# ------------------------------------------------- #
+
+@app.route('/insert_user', methods=['POST'])
+def insert_user_func():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+
+    query = "INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s')" % (name, email, password)
+    interact_db(query=query, query_type='commit')
+    return redirect('/users')
+
+# ------------------------------------------------- #
+# -------------------- INSERT --------------------- #
+# ------------------------------------------------- #
+# @app.route('/insert_user', methods=['GET', 'POST'])
+# def insert_user():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         password = request.form['password']
+#         # recheck
+#         query = "INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s')" % (name, email, password)
+#         interact_db(query=query, query_type='commit')
+#     return redirect(url_for('select_users_func'))
+
+
+# ------------------------------------------------- #
+# ------------------------------------------------- #
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user_func():
+    user_id = request.form['id']
+    query = "DELETE FROM users WHERE id='%s';" % user_id
+    interact_db(query, query_type='commit')
+    return redirect('/users')
+
+# ------------------------------------------------- #
+# -------------------- DELETE --------------------- #
+# ------------------------------------------------- #
+
+# @app.route('/delete_user', methods=['POST'])
+# def delete_user():
+#     user_id = request.form['id']
+#     query = "DELETE FROM users WHERE id='%s';" % user_id
+#     interact_db(query, query_type='commit')
+#     return redirect(url_for('select_users_func'))
+
+
+# ------------------------------------------------- #
+# ------------------------------------------------- #
 
 if __name__ == '__main__':
     app.run(debug=True)
